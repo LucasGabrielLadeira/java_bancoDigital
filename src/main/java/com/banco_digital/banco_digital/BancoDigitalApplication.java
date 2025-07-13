ba2249e649f8b2f5e15b20a0352c5e77d0827885
nimblefx <lucasgabriel.fla@hotmail.com>
2025-07-13 15:03:55 -0300

    2025-07-13

--- src/main/java/com/banco_digital/banco_digital/BancoDigitalApplication.java
+++ src/main/java/com/banco_digital/banco_digital/BancoDigitalApplication.java
@@ -10,17 +10,29 @@ public class BancoDigitalApplication {
 		SpringApplication.run(BancoDigitalApplication.class, args);
 		Banco banco = new Banco("Banco do Brasil");
         Agencia agencia = new Agencia("Rua Portugal, 103", banco);
+        Agencia agencia2 = new Agencia("Rua Pedras de Março, 256", banco);
         Cliente cliente = new Cliente(agencia, "Joao");
         Cliente cliente2 = new Cliente(agencia, "Lucas");
+        Cliente cliente3 = new Cliente(agencia2, "Flavio");
+        Cliente cliente4 = new Cliente(agencia2, "Otavio");
+        Cliente cliente5 = new Cliente(agencia, "Marcelo");
         ContaCorrente conta = new ContaCorrente(agencia, cliente);
         ContaCorrente conta2 = new ContaCorrente(agencia, cliente2);
         conta.depositar(1000);
         conta2.depositar(500);
         conta.transferir(200, conta2);
+        System.out.println("==========");
         conta.imprimirExtrato();
+        System.out.println("==========");
         conta2.imprimirExtrato();
+        System.out.println("==========");
         banco.listarTodasAgencias();
+        System.out.println("==========");
         banco.listarTodosClientes();
+        System.out.println("==========");
+        agencia.listarTodosClientes();
+        System.out.println("==========");
+        agencia2.listarTodosClientes();
 	}
 
 }
